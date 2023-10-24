1217ff7bcad42f1635fce5430a5f8e92c5007038
chiara1911 <chiaracesari89@gmail.com>
2023-10-24 18:39:22 +0200

    add snack4

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const segnaposto = tavoloVip.map((element, index) => {
 return {tableName:'tavoloVip',guestName:element,place:index +1}
 
 });
-console.log(segnaposto)
+console.log(segnaposto);
 
 // ## SNACK 2
 // Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
@@ -84,3 +84,55 @@ const superStudent = studenti.filter((el) =>{
 })
 
 console.log(superStudent);
+
+
+// SNACK 3
+// A partire da un array di stringhe, crea un secondo array formattando le
+// stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+// Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+
+
+const disney =['pippo', 'pluto', 'paperino'];
+   
+console.log(disney);
+
+const upEl = disney.map((element)=>{
+   const sliceUp = element.charAt(0);
+  return sliceUp.toUpperCase() + element.charAt();
+ 
+});
+
+
+// SNACK 4
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+// Crea un nuovo array con la lista dei mammiferi.
+// [
+//  { nome: ‘leone’, famiglia: ‘felidi’, classe: ‘mammiferi’ },
+//  { nome: ‘cane’, famiglia: ‘canidi’, classe: ‘mammiferi’ },
+//  { nome: ‘gallina’, famiglia: ‘fasianidi’, classe: ‘uccelli’ },
+
+const animals = [
+   {
+    nome: 'gatto',
+    famiglia: 'felini',
+    classe: 'mammiferi'
+   },
+   {
+    nome: 'cane',
+    famiglia: 'canidi',
+    classe: 'mammiferi'
+   },
+   {
+    nome: 'pettirosso',
+    famiglia: 'volatili',
+    classe: 'uccelli'
+   }
+];
+
+const mamAnimals = animals.filter((el) => {
+    return el.classe === 'mammiferi';
+    
+});
+console.log(mamAnimals);
