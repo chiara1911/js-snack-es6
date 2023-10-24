20ad0fccfad51ff43dec9936e8d206c28d442e5f
chiara1911 <chiaracesari89@gmail.com>
2023-10-24 16:44:47 +0200

    add array students

--- js/script.js
+++ js/script.js
@@ -20,3 +20,49 @@ return {tableName:'tavoloVip',guestName:element,place:index +1}
 
 });
 console.log(segnaposto)
+
+// ## SNACK 2
+// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+// 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+// ES (Marco della Rovere => MARCO DELLA ROVERE);
+// 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+// 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+// creo array studenti
+const studenti =[
+    {
+        id: 213,
+        name : 'Marco della Rovere',
+        grades : 78
+    },
+    {
+        id: 110,
+        name : 'Paola Cortellessa',
+        grades : 96
+    },
+    {
+        id: 250,
+        name : 'Andrea Mantegna',
+        grades : 48
+    },
+    {
+        id: 145,
+        name : 'Gaia Borromini',
+        grades : 74
+    },
+    {
+        id: 196,
+        name : 'Luigi Grimaldello',
+        grades : 68
+    },
+    {
+        id: 102,
+        name : 'Piero della Francesca',
+        grades : 50
+    },
+    {
+        id: 120,
+        name : 'Francesca da Polenta',
+        grades : 84
+    }
+]
