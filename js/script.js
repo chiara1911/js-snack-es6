4cce1571280e08a80665fc356769fe8297d294fb
chiara1911 <chiaracesari89@gmail.com>
2023-10-25 17:32:55 +0200

    add football team array

--- js/script.js
+++ js/script.js
@@ -249,9 +249,45 @@ console.log(biciLeggera);
 console.log(` questa è la bici più leggera. Si chiama ${biciLeggera.nome} e pesa ${biciLeggera.peso} kg`);
 
 // ## SNACK 4
-// Creare un array di oggetti di squadre di calcio.
-// > Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+
 
 // - Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // > Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 // - Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// Creare un array di oggetti di squadre di calcio.
+// > Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+
+
+// Creare un array di oggetti di squadre di calcio.
+const squadre = [
+    {
+        nome: 'Bologna',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Chievo',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0
+    }
+]
+
+
+
+
+// utility
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
