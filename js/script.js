aaf0672e2805e5f51e1483b3945089f81d06165b
chiara1911 <chiaracesari89@gmail.com>
2023-10-25 14:55:08 +0200

    add array bike

--- js/script.js
+++ js/script.js
@@ -8,18 +8,42 @@
 // Generiamo e stampiamo in console la lista per i segnaposto.
 
 // creo array con i nomi
-const tavoloVip =[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
 
 
 
+const tavoloVip = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
-const segnaposto = tavoloVip.map((element, index) => {  
 
-// return 'nome' +  element + '' + 'posto:' + index++
-return {tableName:'tavoloVip',guestName:element,place:index +1}
 
+const segnaposto = [];
+person = tavoloVip.map((element, index) => {
+
+
+    return { tableName: 'tavoloVip', guestName: element, place: index + 1 }
 });
+
 console.log(segnaposto);
+let place = 1
+tavoloVip.forEach((element, index, array)=> {
+ array[index]= {
+    tableName: 'tavoloVip',
+     guestName: element,
+      place: place
+ }
+ if (guest ==='Maneskin'){
+    for(let i= 0; i < 3; i++){
+        place++;
+        array.push({
+            tableName: 'tavoloVip',
+             guestName: element,
+              place: place
+         })
+    }
+ }
+
+})
+
+
 
 // ## SNACK 2
 // Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
@@ -29,57 +53,57 @@ console.log(segnaposto);
 // 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 
 // creo array studenti
-const studenti =[
+const studenti = [
     {
         id: 213,
-        name : 'Marco della Rovere',
-        grades : 78
+        name: 'Marco della Rovere',
+        grades: 78
     },
     {
         id: 110,
-        name : 'Paola Cortellessa',
-        grades : 96
+        name: 'Paola Cortellessa',
+        grades: 96
     },
     {
         id: 250,
-        name : 'Andrea Mantegna',
-        grades : 48
+        name: 'Andrea Mantegna',
+        grades: 48
     },
     {
         id: 145,
-        name : 'Gaia Borromini',
-        grades : 74
+        name: 'Gaia Borromini',
+        grades: 74
     },
     {
         id: 196,
-        name : 'Luigi Grimaldello',
-        grades : 68
+        name: 'Luigi Grimaldello',
+        grades: 68
     },
     {
         id: 102,
-        name : 'Piero della Francesca',
-        grades : 50
+        name: 'Piero della Francesca',
+        grades: 50
     },
     {
         id: 120,
-        name : 'Francesca da Polenta',
-        grades : 84
+        name: 'Francesca da Polenta',
+        grades: 84
     }
 ]
 // Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
-const upperName = studenti.map((el) =>{
-  console.log(el.name.toUpperCase());
+const upperName = studenti.map((el) => {
+    console.log(el.name.toUpperCase());
 })
 // Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
- 
-const goodStudent = studenti.filter((el) =>{
+
+const goodStudent = studenti.filter((el) => {
     return el.grades > 70;
 })
 console.log(goodStudent);
 
 // Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 
-const superStudent = studenti.filter((el) =>{
+const superStudent = studenti.filter((el) => {
     return el.grades > 70 && el.id > 120;
 })
 
@@ -92,16 +116,16 @@ console.log(superStudent);
 // Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
 
 
-const disney =['pippo', 'pluto', 'paperino'];
-   
-console.log(disney);
+const disney = ['pippo', 'pluto', 'paperino'];
 
-const upEl = disney.map((element)=>{
-   const sliceUp = element.charAt(0);
-  return sliceUp.toUpperCase() + element.charAt();
- 
+
+
+const upEl = disney.map((element) => {
+    const sliceUp = element.charAt(0);
+    return sliceUp.toUpperCase() + sliceUp.slice(1);
 });
 
+console.log(upEl)
 
 // SNACK 4
 // Crea un array di oggetti che rappresentano degli animali.
@@ -114,25 +138,97 @@ const upEl = disney.map((element)=>{
 //  { nome: ‘gallina’, famiglia: ‘fasianidi’, classe: ‘uccelli’ },
 
 const animals = [
-   {
-    nome: 'gatto',
-    famiglia: 'felini',
-    classe: 'mammiferi'
-   },
-   {
-    nome: 'cane',
-    famiglia: 'canidi',
-    classe: 'mammiferi'
-   },
-   {
-    nome: 'pettirosso',
-    famiglia: 'volatili',
-    classe: 'uccelli'
-   }
+    {
+        nome: 'gatto',
+        famiglia: 'felini',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'pettirosso',
+        famiglia: 'volatili',
+        classe: 'uccelli'
+    }
 ];
 
 const mamAnimals = animals.filter((el) => {
     return el.classe === 'mammiferi';
-    
+
 });
 console.log(mamAnimals);
+
+// Crea un array di oggetti che rappresentano delle persone.
+// Ogni persona ha un nome, un cognome e un’età.
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il
+// nome e cognome e l’indicazione se può guidare, in base all’età.
+
+const family = [
+    {
+        nome: 'Gina',
+        cognome: 'Lollo',
+        anni: 81
+    },
+    {
+        nome: 'Primo',
+        cognome: 'Ultimo',
+        anni: 16
+    },
+    {
+        nome: 'Felice',
+        cognome: 'Povero',
+        anni: 41
+    },
+    {
+        nome: 'Assunta',
+        cognome: 'Triste',
+        anni: 34
+    },
+
+]
+
+const people = family.map((element) => {
+
+    let text;
+    if (element.anni > 18) {
+        text = 'può guidare';
+    } else {
+        text = 'non può guidare';
+    }
+
+    return `il componente con nome ${element.nome} e cognome ${element.cognome} e ${element.anni} ${text} `
+});
+console.log(people);
+
+
+// ## SNACK 3
+// Creare un array di oggetti:
+// - Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// - Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+
+// creare array bici da corsa
+
+const bici =[
+    {
+        nome: 'Flash',
+        peso: 7
+    },
+    {
+        nome: 'Turbo',
+        peso: 6
+    },
+    {
+        nome: 'Giulietta',
+        peso: 8
+    },
+    {
+        nome: 'Postino',
+        peso: 9
+    }
+];
+
+
